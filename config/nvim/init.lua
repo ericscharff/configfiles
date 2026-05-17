d45f3e328e4ffd58fbfdfacab6fa22bce2e28805
Eric Scharff <eric.scharff@gmail.com>
2026-05-17 16:52:06 -0600

    Always set termguicolors
    All my terminals support 24 bit color. neovim doesn't always auto detect
    it.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,6 +4,7 @@ vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.cursorline = true
 vim.opt.number = true
+vim.opt.termguicolors = true
 vim.diagnostic.config({ virtual_text = true })
 
 vim.g.mapleader = ','
