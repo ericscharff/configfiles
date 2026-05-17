ae5ba870af1c3ecec1cdb66dbd632e69e0df74a4
Eric Scharff <eric.scharff@gmail.com>
2026-05-17 14:53:07 -0600

    Add minimalist typescript language server config
    This should work in a project that has a package.json, and runs the
    (locally installed) typescript-language-server using npx.

---
+++ config/nvim/lsp/ts_ls.lua
@@ -0,0 +1,27 @@
+return {
+  cmd = { 'npx', '--no-install', 'typescript-language-server', '--stdio' },
+
+  filetypes = {
+    'javascript',
+    'javascriptreact',
+    'typescript',
+    'typescriptreact',
+  },
+
+  root_markers = { 'package.json' },
+
+  -- Specific settings to send to the server. The schema for this is
+  -- defined by the server. For example the schema for lua-language-server
+  -- can be found here https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json
+  -- settings = {
+  --   Lua = {
+  --     runtime = {
+  --       version = 'LuaJIT',
+  --     },
+  --     diagnostics = {
+  --       -- Get the language server to recognize the `vim` global
+  --       globals = { 'vim' },
+  --     },
+  --   }
+  -- }
+}
