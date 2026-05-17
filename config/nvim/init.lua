7d828447ba4933670ed5136fa40ef4d5ea82a77c
Eric Scharff <eric.scharff@gmail.com>
2026-05-17 07:57:17 -0600

    Install initial LSP support
    This uses the official nvim-lspconfig package to install default LSP
    mappings for language servers. This is a minimalist setup that assumes
    that the language servers are already installed separately.
    
    This currently only enables the lua language server, and customizes it a
    bit to avoid vim warnings, per the documentation in the file.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,6 +4,7 @@ vim.opt.softtabstop = 2
 vim.opt.expandtab = true
 vim.opt.cursorline = true
 vim.opt.number = true
+vim.diagnostic.config({ virtual_text = true })
 
 vim.g.mapleader = ','
 
@@ -22,3 +23,5 @@ vim.filetype.add({
 })
 
 require("config.lazy")
+
+vim.lsp.enable({"lua_ls"})
