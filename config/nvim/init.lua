0dd7cb1dceb1a3d06842b0357d677cd2c250cea4
Eric Scharff <eric.scharff@gmail.com>
2026-05-17 19:35:08 -0600

    Make keymaps more compact

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -11,25 +11,12 @@ vim.diagnostic.config({ virtual_text = true })
 vim.g.mapleader = ","
 vim.g.have_nerd_font = true
 
-vim.keymap.set(
-  "n",
-  "<leader>cc",
-  '"+yy',
-  { desc = "Copy line to system clipboard" }
-)
-vim.keymap.set(
-  "v",
-  "<leader>c",
-  '"+y',
-  { desc = "Copy selection to system clipboard" }
-)
-
-vim.keymap.set(
-  "n",
-  "<leader>p",
-  "<cmd>%!pretty %<CR>",
-  { desc = "Run prettier on current file" }
-)
+vim.keymap.set("n", "<leader>cc", '"+yy',
+  { desc = "Copy line to system clipboard" })
+vim.keymap.set("v", "<leader>c", '"+y',
+  { desc = "Copy selection to system clipboard" })
+vim.keymap.set("n", "<leader>p", "<cmd>%!pretty %<CR>",
+  { desc = "Run prettier on current file" })
 
 vim.filetype.add({
   extension = {
