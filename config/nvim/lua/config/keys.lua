59acc135182d4f2e3db2896245743b86a6ba6d45
Eric Scharff <eric.scharff@gmail.com>
2026-05-18 20:51:29 -0600

    Map Shift-Left and Shift-Right for wondow navigation
    These are shortcuts for bnext and bprev. The arrows are a bit awkward
    but perhaps better than needing commands from command mode.

--- config/nvim/lua/config/keys.lua
+++ config/nvim/lua/config/keys.lua
@@ -6,3 +6,7 @@ vim.keymap.set('v', '<leader>c', '"+y',
   { desc = 'Copy selection to system clipboard' })
 vim.keymap.set('n', '<leader>p', '<cmd>%!pretty %<CR>',
   { desc = 'Run prettier on current file' })
+vim.keymap.set('n', '<S-Left>', ':bprev<CR>',
+  { desc = "Jump to previous buffer", silent = true })
+vim.keymap.set('n', '<S-Right>', ':bnext<CR>',
+  { desc = "Jump to next buffer", silent = true })
