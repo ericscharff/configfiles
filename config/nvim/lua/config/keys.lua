9c3697b725143c6dea314c720b3f34b8a75862a0
Eric Scharff <eric.scharff@gmail.com>
2026-05-18 20:58:34 -0600

    Switch shoft to home row keys
    I think this is more ergonomic, espeically on a tiny keyboard like the
    chromebook.

--- config/nvim/lua/config/keys.lua
+++ config/nvim/lua/config/keys.lua
@@ -6,7 +6,7 @@ vim.keymap.set('v', '<leader>c', '"+y',
   { desc = 'Copy selection to system clipboard' })
 vim.keymap.set('n', '<leader>p', '<cmd>%!pretty %<CR>',
   { desc = 'Run prettier on current file' })
-vim.keymap.set('n', '<S-Left>', ':bprev<CR>',
+vim.keymap.set('n', '<leader>h', ':bprev<CR>',
   { desc = "Jump to previous buffer", silent = true })
-vim.keymap.set('n', '<S-Right>', ':bnext<CR>',
+vim.keymap.set('n', '<leader>l', ':bnext<CR>',
   { desc = "Jump to next buffer", silent = true })
