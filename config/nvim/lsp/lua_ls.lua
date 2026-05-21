ded0ab92228ef2ad11c42e990ca93969b38a4941
Eric Scharff <eric.scharff@gmail.com>
2026-05-21 14:06:20 -0600

    Attempt to get better lua LSP support
    this is somewhat working, but I would have expected more functions to be
    autocompleted, and more definitions.

--- config/nvim/lsp/lua_ls.lua
+++ config/nvim/lsp/lua_ls.lua
@@ -7,12 +7,16 @@ return {
 
   settings = {
     Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
       diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = { 'vim' },
+        globals = { "vim" }, -- Recognize 'vim' as a global variable
+      },
+      workspace = {
+        library = {
+          vim.api.nvim_get_runtime_file("", true),
+        }, -- Include Neovim runtime files
+      },
+      telemetry = {
+        enable = false,
       },
     },
   },
